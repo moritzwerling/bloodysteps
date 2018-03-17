96b8a707f9ab7fe29e3fe63db78fef1f7dcc7ee4
Moritz Werling <moritzwerling@gmail.com>
2018-03-17 22:37:55 +0100

    Add simple test for simulation

--- bgv/src/sim_dotm.py
+++ bgv/src/sim_dotm.py
@@ -1,6 +1,20 @@
 import numpy as np
-def fun(x,t,v): # x_dot = f(x,t)
-        return [0,0,0,0,0]
+from scipy.integrate import odeint
 
-def simulate(x0, delta_t, n_steps):
-        return np.zeros((5,n_steps))
+
+def fun(x,t,v): # x_dot = f(x,t)
+
+    psi = 0
+    beta = 0
+    theta = psi + beta
+    dxdt = [v*np.cos(theta), #x
+            v*np.sin(theta), #y
+            0, #psi
+            0, #r
+            0] #beta
+    return dxdt
+
+
+def simulate(x0, v, ti):
+    sol = odeint(fun, x0, ti, args=(v,))
+    return sol
--- bgv/test/sim_dotm_test.py
+++ bgv/test/sim_dotm_test.py
@@ -1,6 +1,6 @@
 import sim_dotm as dotm
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_almost_equal
 
 
 def test_fun():
@@ -12,10 +12,21 @@ def test_fun():
     assert(state_x_dot == [0,0,0,0,0])
 
 
-def test_simulate():
-    x0 = []
+def test_simulate_standstill():
+    x0 = [0,0,0,0,0]
     delta_t = 0.01
-    n_steps = 100
-    x_actual = dotm.simulate(x0, delta_t, n_steps)
-    x_desired = np.zeros((5,n_steps))
-    assert_array_equal(x_actual, x_desired)
+    ti = np.linspace(0,1,10)
+    v = 0
+    x_desired = np.zeros((len(ti),5))
+    x_actual = dotm.simulate(x0, v, ti)
+    np.testing.assert_allclose(x_desired,x_actual)
+
+
+def test_simulate_const_velocity():
+    x0 = [0,0,0,0,0]
+    ti = np.linspace(0,1,10)
+    v = 10
+    x_desired = np.zeros((len(ti),5))
+    x_actual = dotm.simulate(x0, v, ti)
+    x_desired[:,0] = ti*v
+    np.testing.assert_allclose(x_desired,x_actual,rtol=1e-5, atol=0)
