c357a9bd175f298a665d023c47fff75ff990ffd3
Moritz Werling <moritzwerling@gmail.com>
2018-03-31 00:13:59 +0200

    Make circle movable

--- bgv/src/plot_kamms_circle.py
+++ bgv/src/plot_kamms_circle.py
@@ -3,20 +3,22 @@ from matplotlib.patches import Circle
 
 
 class KammsCircle(object):
-    def __init__(self, ax, a_max=10):
-        c = Circle((0, 0), a_max, color='grey')
+    def __init__(self, ax, x=0, y=0, a_max=10):
+        self.x = x
+        self.y = y
+        c = Circle((x, y), a_max, facecolor='grey', edgecolor='k')
         ax.add_patch(c)
         self.acc = plt.Line2D([], [], color='k', marker='P')
         ax.add_line(self.acc)
 
     def update(self, a_vec):
-        self.acc.set(xdata=[0, a_vec[0]], ydata=[0, a_vec[1]])
+        self.acc.set(xdata=[self.x, self.x+a_vec[0]], ydata=[self.y, self.y+a_vec[1]])
 
 
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111, aspect='equal')
 
-kc = KammsCircle(ax1)
-kc.update((5, 5))
+kc = KammsCircle(ax1, 3, 5)
+kc.update((-2, 5))
 plt.margins(.5)
 plt.show()
