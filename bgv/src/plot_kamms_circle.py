61a4442a748516ba588b441e41f8e9470ddaf48c
Moritz Werling <moritzwerling@gmail.com>
2018-04-04 20:05:35 +0200

    Normalize Kamms Circle

--- bgv/src/plot_kamms_circle.py
+++ bgv/src/plot_kamms_circle.py
@@ -3,16 +3,18 @@ from matplotlib.patches import Circle
 
 
 class KammsCircle(object):
-    def __init__(self, ax, x=0, y=0, a_max=10):
+    def __init__(self, ax, x=0, y=0, r=1, a_max=10):
         self.x = x
         self.y = y
-        c = Circle((x, y), a_max, facecolor='grey', edgecolor='k')
+        self.r = r
+        self.a_max = a_max
+        c = Circle((x, y), r, facecolor='grey', edgecolor='k')
         ax.add_patch(c)
-        self.acc = plt.Line2D([], [], color='k', marker='P')
-        ax.add_line(self.acc)
+        self.a = plt.Line2D([], [], color='k', marker='P')
+        ax.add_line(self.a)
 
     def update(self, a_vec):
-        self.acc.set(xdata=[self.x, self.x+a_vec[0]], ydata=[self.y, self.y+a_vec[1]])
+        self.a.set(xdata=[self.x, self.x + a_vec[0]/self.a_max*self.r], ydata=[self.y, self.y + a_vec[1]/self.a_max*self.r])
 
 
 fig1 = plt.figure()
