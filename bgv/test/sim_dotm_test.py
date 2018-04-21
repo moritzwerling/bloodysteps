a98ffbfb4c34718846d44ad60ebcd2e73f8a506e
Moritz Werling <moritzwerling@gmail.com>
2018-04-21 09:33:08 +0200

    Minor formatting

--- bgv/test/sim_dotm_test.py
+++ bgv/test/sim_dotm_test.py
@@ -7,29 +7,28 @@ class TestSimDotm(object):
         x, y, psi, beta, r = 0, 0, 0, 0, 0
         t = 0
         v = 10
-        state_x = [x,y,psi,beta,r]
-        state_x_dot = dotm.fun(state_x,v,t)
-        assert(state_x_dot == [0,0,0,0,0])
-
+        state_x = [x, y, psi, beta, r]
+        state_x_dot = dotm.fun(state_x, v, t)
+        assert(state_x_dot == [0, 0, 0, 0, 0])
 
     def test_simulate_standstill(self):
-        x0 = [0,0,0,0,0]
-        ti = np.linspace(0,1,10)
+        x0 = [0, 0, 0, 0, 0]
+        ti = np.linspace(0, 1, 10)
         v = 0
-        x_desired = np.zeros((len(ti),5))
+        x_desired = np.zeros((len(ti), 5))
         x_actual = dotm.simulate(x0, v, ti)
-        np.testing.assert_allclose(x_desired,x_actual)
+        np.testing.assert_allclose(x_desired, x_actual)
 
 
     def test_simulate_const_velocity(self):
-        x0 = [0,0,0,0,0]
-        ti = np.linspace(0,1,10)
+        x0 = [0, 0, 0, 0, 0]
+        ti = np.linspace(0, 1, 10)
         v = 10
-        x_desired = np.zeros((len(ti),5))
-        x_desired[:,0] = ti*v
+        x_desired = np.zeros((len(ti), 5))
+        x_desired[:, 0] = ti*v
         x_actual = dotm.simulate(x0, v, ti)
 
-        np.testing.assert_allclose(x_desired,x_actual,rtol=1e-5, atol=0)
+        np.testing.assert_allclose(x_desired, x_actual, rtol=1e-5, atol=0)
 
 
     def test_simulate_const_velocity_diagonal(self):
--- bgv/test/sim_kotm_test.py
+++ bgv/test/sim_kotm_test.py
@@ -1,13 +1,19 @@
 import sim_kotm as kotm
 import numpy as np
 import numpy.testing as npt
+from scipy.interpolate import interp1d
 
 
 class TestSimKotm(object):
     def test_simulate_const_velocity_diagonal(self):
         psi_const = np.pi / 4
         x0 = [0, 0, psi_const]
-        delta = 0
+        delta0 = 0
+
+        t = np.array([0., 0.])
+        delta = np.array([delta0, delta0])
+        delta_of_t = interp1d(t, delta, kind='linear', fill_value='extrapolate')
+        delta_test = delta_of_t([0.0, ])
         ti = np.linspace(0, 1, 10)
         v = 5
         s = ti * v
@@ -15,7 +21,7 @@ class TestSimKotm(object):
         x_desired[:, 0] = s * np.cos(psi_const)
         x_desired[:, 1] = s * np.sin(psi_const)
         x_desired[:, 2] = np.ones(s.shape) * psi_const
-        x_actual = kotm.simulate(x0, v, delta, ti)
+        x_actual = kotm.simulate(x0, v, delta_of_t, ti)
 
         np.testing.assert_allclose(x_desired, x_actual, rtol=1e-5, atol=0)
 
