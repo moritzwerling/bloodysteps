736f0aa319cae0118c2b88c1e059f866378f1d66
Moritz Werling <moritzwerling@gmail.com>
2018-04-21 21:16:26 +0200

    Add projection function

--- bgv/src/pseudo_projection.py
+++ bgv/src/pseudo_projection.py
@@ -1,13 +1,62 @@
+from numpy import linalg as LA
+import numpy as np
+import kinematic_control as kc
+
 
 def project2curve(s_c, x_c, y_c, theta_c, kappa_c, x, y):
-    x_p = 0.5
-    y_p = 0.
-    s_p = 0.5
-    d = 1.0
-    theta_p = 0.
-    kappa_p = 0.
+    p1 = [x_c[0], y_c[0]]
+    p2 = [x_c[1], y_c[1]]
+    theta1 = theta_c[0]
+    theta2 = theta_c[1]
+    s1 = s_c[0]
+    s2 = s_c[1]
+    mindex = 1
 
-    return [x_p, y_p, s_p, d, theta_p, kappa_p]
+    px_, lambda_, sign = pseudo_projection([x, y], p1, p2, theta1, theta2)
+    x_p = px_[0]
+    y_p = px_[1]
+
+    d = sign * np.sqrt((x_p - x)**2 + (y_p - y)**2)
+    s_p = lambda_ * s2 + (1.0 - lambda_) * s1
+    kappa1 = kappa_c[mindex - 1]
+    kappa2 = kappa_c[mindex]
+    delta_theta = theta2 - theta1
+    delta_theta = kc.normalize_angle(delta_theta)
+    theta_p = theta1 + lambda_ * delta_theta
+    theta_p = kc.normalize_angle(theta_p)
+    kappa_p = lambda_ * kappa2 + (1.0 - lambda_) * kappa1;
+
+    return [px_[0], px_[1], s_p, d, theta_p, kappa_p]
+
+
+def pseudo_projection(x, p1, p2, theta1, theta2):
+    delta = np.array(p2) - np.array(p1)
+    l = LA.norm(delta)
+
+    # transform so that origin is p1 oriented to p2
+    alpha = np.arctan2(delta[1], delta[0])
+    sin_ = np.sin(-alpha)
+    cos_ = np.cos(-alpha)
+    R = np.array([[cos_, -sin_],
+                  [sin_,  cos_]])
+    x_ = np.dot(R, (np.array(x)-np.array(p1)))
+    m1 = np.tan(theta1-alpha)
+    m2 = np.tan(theta2-alpha)
+    devi = (m1 - m2) * x_[1] + l
+    if abs(devi) > 0.001:
+        lambda_ = (m1 * x_[1] + x_[0]) / devi
+    else:
+        lambda_ = .5
+
+    px = lambda_ * np.array(p2) + (1.0-lambda_) * np.array(p1)
+
+    sgn = 0
+    if x_[1] > 0:
+        sgn = 1
+    elif x_[1] < 0:
+        sgn = -1
+
+    return [px, lambda_, sgn]
 
 
 
--- bgv/test/pseudo_projection_test.py
+++ bgv/test/pseudo_projection_test.py
@@ -1,6 +1,11 @@
+import numpy as np
 import pseudo_projection as pp
+import pytest
+
 
 class TestPseudoProjection(object):
+
+    #@pytest.mark.skip(reason="no way of currently testing this")
     def test_two_points(self):
         x_c = [0, 1]
         y_c = [0, 0]
@@ -10,7 +15,22 @@ class TestPseudoProjection(object):
 
         x = .5
         y = 1
-        point_proj_desired = [0.5, 0, 0.5, 1.0, 0.0, 0.0]
-        point_proj_actual = pp.project2curve(x_c, y_c, s_c, theta_c, kappa_c, x, y)
-        assert point_proj_desired == point_proj_actual
+        proj_desired = [0.5, 0, 0.5, 1.0, 0.0, 0.0]
+        proj_actual = pp.project2curve(s_c, x_c, y_c, theta_c, kappa_c, x, y)
+        assert proj_desired == proj_actual
+
+    def test_two_points_diagonal(self):
+        x_c = [0, 1]
+        y_c = [0, 1]
+        s_c = [0, np.sqrt(2)]
+        theta_c = [np.pi/4, np.pi/4]
+        kappa_c = [0, 0]
+
+        x = 0.
+        y = 1.
+        proj_desired = [0.5, 0.5, 0.5*np.sqrt(2), 0.5*np.sqrt(2), np.pi/4, 0.0]
+        proj_actual = pp.project2curve(s_c, x_c, y_c, theta_c, kappa_c, x, y)
+        np.testing.assert_allclose(proj_desired, proj_actual)
+
+
 
