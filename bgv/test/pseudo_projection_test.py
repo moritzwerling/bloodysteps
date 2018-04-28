66865eb81da09e55e101dc43e01d8e1e096091ec
Moritz Werling <moritzwerling@gmail.com>
2018-04-28 22:25:02 +0200

    Add failing test for projection

--- bgv/test/pseudo_projection_test.py
+++ bgv/test/pseudo_projection_test.py
@@ -1,6 +1,7 @@
 import numpy as np
-import pseudo_projection as pp
+from numpy import linalg as LA
 import pytest
+import pseudo_projection as pp
 
 
 class TestPseudoProjection(object):
@@ -57,6 +58,34 @@ class TestPseudoProjection(object):
         proj_desired = [0.6, 0, 1.6, 1.0, 0.0, 0.0]
         proj_actual = pp.project2curve(s_c, x_c, y_c, theta_c, kappa_c, x, y)
         np.testing.assert_allclose(proj_desired, proj_actual)
+
+    @pytest.mark.skip(reason="Do not know yet why this test is failing.")
+    def test_inverse_problem_zero_distance(self):
+        x_c = [1.2, 2.1]
+        y_c = [3.2, 1.1]
+        s_c = [5.2, 5.2 + np.sqrt((x_c[1] - x_c[0])**2 + (y_c[1] - y_c[0])**2)]
+        theta_c = [1.2, 2.3]
+        kappa_c = [3.3, 4.4]
+
+        lambda_ = 0.4
+        d_desired = -1.3
+
+        x_desired, y_desired, s_desired, cos_theta_desired, sin_theta_desired, kappa_desired = \
+            (1.0 - lambda_)*np.array([x_c[1], y_c[1], s_c[1], np.cos(theta_c[1]), np.sin(theta_c[1]), kappa_c[1]]) \
+                + lambda_ * np.array([x_c[0], y_c[0], s_c[0], np.cos(theta_c[0]), np.sin(theta_c[1]), kappa_c[0]])
+
+        theta_desired = np.arccos(cos_theta_desired)
+
+        T = np.array([cos_theta_desired, sin_theta_desired])
+        T_unit = T / LA.norm(T)
+        N_unit = np.array([- T_unit[1], T[0]])
+        x, y = np.array([x_desired, y_desired]) + d_desired * N_unit
+
+        proj_desired = [x_desired, y_desired, s_desired, d_desired, theta_desired, kappa_desired]
+        proj_actual = pp.project2curve(s_c, x_c, y_c, theta_c, kappa_c, x, y)
+        np.testing.assert_allclose(proj_desired, proj_actual)
+
+
 
 
 
