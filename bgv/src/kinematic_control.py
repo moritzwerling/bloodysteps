8551f8d001fde3f24d220b11eac6fbde8d2e3b3a
Moritz Werling <moritzwerling@gmail.com>
2018-04-30 13:28:49 +0200

    Add closed-loop function and test

--- bgv/src/kinematic_control.py
+++ bgv/src/kinematic_control.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pseudo_projection as pro
+import sim_kotm as kotm
 
 
 def feedback_law(d, psi, theta_r, kappa_r):
@@ -16,4 +18,15 @@ def feedback_law(d, psi, theta_r, kappa_r):
 
 def normalize_angle(angle):
     return (angle+np.pi) % (2*np.pi) - np.pi
+
+
+def f_closed_loop(x_, t, curve):
+    x, y, psi = x_
+    _, _, _, d, theta_r, kappa_r = \
+        pro.project2curve(curve['s'], curve['x'], curve['y'], curve['theta'], curve['kappa'],
+                          x, y)
+    delta = feedback_law(d, psi, theta_r, kappa_r)
+    v = 1  # const velocity
+    x_dot_ = kotm.fun(x_, t, v, delta)
+    return x_dot_
 
--- bgv/test/kinematic_control_test.py
+++ bgv/test/kinematic_control_test.py
@@ -1,5 +1,7 @@
 import numpy as np
+from scipy.integrate import odeint
 import kinematic_control as kc
+import generate_reference_curve as ref
 
 
 def test_normalize_angle():
@@ -8,6 +10,7 @@ def test_normalize_angle():
     np.testing.assert_almost_equal(kc.normalize_angle(2*np.pi + 0.1), 0.1, decimal=10)
     np.testing.assert_almost_equal(kc.normalize_angle(2*np.pi - 0.1), -0.1, decimal=10)
     np.testing.assert_almost_equal(kc.normalize_angle(np.pi + 0.1), -np.pi + 0.1, decimal=10)
+
 
 def test_kinematic_control_law():
     # delta = feedback_law(d, psi, theta_r, kappa_r)
@@ -19,4 +22,22 @@ def test_kinematic_control_law():
     assert kc.feedback_law(-0.1, 0, 0, 0) > 0
     assert kc.feedback_law(0, 0.1, 0.1, 0.1) > 0.0
     assert kc.feedback_law(0, 0.1, 0.1, -0.1) < 0.0
+
+
+def test_closed_loop_straigt_without_initial_error():
+    x0 = [0.0, 0.0, 0.0]
+    curve = ref.generate_reference_curve([0, 10, 20, 30], [0, 0, 0, 0], 1.0)
+    ti = np.arange(0, 20, 0.1)
+    sol = odeint(kc.f_closed_loop, x0, ti, (curve,))
+    y = sol[:, 1]
+    np.testing.assert_almost_equal(0.0, y[-1])
+
+
+def test_closed_loop_straigt_with_initial_error():
+    x0 = [0.0, .1, 0.0]
+    curve = ref.generate_reference_curve([0, 10, 20, 30], [0, 0, 0, 0], 1.0)
+    ti = np.arange(0, 20, 0.1)
+    sol = odeint(kc.f_closed_loop, x0, ti, (curve,))
+    y = sol[:, 1]
+    np.testing.assert_almost_equal(0.0, y[-1], decimal=3)
 
